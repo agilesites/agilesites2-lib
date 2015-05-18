1ce285ad4ae50859becce45dc7abd4d97f94b66e
Michele Sciabarra <michele+youtrack@sciabarra.com>
2015-05-18 18:07:51 +0200

    patch to implement classloader with publishable jar

--- core/src/main/java/wcs/core/Dispatcher.java
+++ core/src/main/java/wcs/core/Dispatcher.java
@@ -1,22 +1,13 @@
 package wcs.core;
 
-import COM.FutureTense.Interfaces.ICS;
-import com.fatwire.assetapi.def.AssetTypeDefManager;
-import com.fatwire.assetapi.site.Site;
-import com.fatwire.assetapi.site.SiteInfo;
-import com.fatwire.assetapi.site.SiteManager;
-import com.fatwire.system.Session;
-import com.fatwire.system.SessionFactory;
+import java.io.File;
+import java.util.StringTokenizer;
+
 import wcs.api.Call;
 import wcs.api.Element;
 import wcs.api.Log;
 import wcs.api.Router;
-
-import java.io.File;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.util.StringTokenizer;
+import COM.FutureTense.Interfaces.ICS;
 
 public class Dispatcher {
 
@@ -35,7 +26,6 @@ public class Dispatcher {
      * @return
      */
     static Dispatcher getDispatcher(ICS ics) {
-
 
         if (dispatcher == null) {
             String jarPath = ics.GetProperty("agilesites.dir");
@@ -57,8 +47,8 @@ public class Dispatcher {
 
             File jarDir = new File(jarPath);
             File assetJarDir = new File(new File(storagePath), "Jar");
-            jarDir.mkdirs();
-            assetJarDir.mkdirs();
+            //jarDir.mkdirs();
+            //assetJarDir.mkdirs();
             log.debug("[Dispatcher.getDispatcher] dir=%s asset=%s defaultpoll=%d", jarDir, assetJarDir,
                     defaultPollInterval);
             dispatcher = new Dispatcher(jarDir, assetJarDir);
@@ -226,90 +216,5 @@ public class Dispatcher {
             }
         }
         return msg.toString();
-    }
-
-    /**
-     * Ensure there is a Jar asset before uploading it
-     * <p/>
-     * Return sites where the jar must be uploaded (at least AdminSite)
-     */
-    public static String ensureJarAsset(ICS ics, String sites, String username, String password) {
-        try {
-            // load the required managers
-            Session ses = SessionFactory.newSession(username, password);
-            AssetTypeDefManager atdm = (AssetTypeDefManager) ses
-                    .getManager(AssetTypeDefManager.class.getName());
-            SiteManager sim = (SiteManager) ses.getManager(SiteManager.class
-                    .getName());
-
-            // create the Jar Asset type if it does not exits
-            boolean existJarAsset = false;
-            Iterator<String> it = atdm.getAssetTypes().iterator();
-            while (it.hasNext()) {
-                if (it.next().equalsIgnoreCase("Jar")) {
-                    existJarAsset = true;
-                    break;
-                }
-            }
-
-            if (!existJarAsset) {
-                File base = new File(ics.GetProperty("xcelerate.defaultbase",
-                        "futuretense_xcel.ini", true));
-                File baseJar = new File(base, "Jar");
-                StringBuilder sb = new StringBuilder();
-                sb.append("<?xml version=\"1.0\" ?>\n");
-                sb.append("<ASSET NAME=\"Jar\" DESCRIPTION=\"Jar\" DEFDIR=\"").append(baseJar.getAbsolutePath()).append("\">\n");
-                sb.append("    <PROPERTIES>\n");
-                sb.append("        <PROPERTY NAME=\"URL\" DESCRIPTION=\"URL\">\n");
-                sb.append("            <STORAGE TYPE=\"VARCHAR\" LENGTH=\"255\" />\n");
-                sb.append("            <INPUTFORM TYPE=\"UPLOAD\" WIDTH=\"64\" LINKTEXT=\"Url\" REQUIRED=\"YES\"/>\n");
-                sb.append("        </PROPERTY>\n");
-                sb.append("    </PROPERTIES>\n");
-                sb.append("</ASSET>\n");
-                atdm.createAssetMakerAssetType("Jar", "Jar.xml",
-                        sb.toString(), false, false);
-            }
-
-            /// enable the Jar type in the requires sites
-
-            // ensure AdminSite is in the list
-            if (sites.indexOf("AdminSite") == -1)
-                sites = sites + ",AdminSite";
-            if (sites.startsWith(","))
-                sites = sites.substring(1);
-
-            // enable jar
-            StringBuffer sb = new StringBuffer();
-            StringTokenizer st = new StringTokenizer(sites, ",");
-            while (st.hasMoreTokens()) {
-                String siteName = st.nextToken();
-                boolean exist = false;
-                for (SiteInfo sif : sim.list()) {
-                    if (sif.getId().equals(siteName)) {
-                        Site site = sim.read(Arrays.asList(siteName)).get(0);
-                        List<String> types = site.getAssetTypes();
-                        boolean hasJar = false;
-                        for (String type : types)
-                            if (type.equals("Jar")) {
-                                hasJar = true;
-                                break;
-                            }
-                        if (!hasJar) {
-                            types.add("Jar");
-                            site.setAssetTypes(types);
-                            sim.update(Arrays.asList(site));
-                            sb.append(siteName).append(",");
-                        }
-                    }
-                }
-            }
-            if (sb.length() > 0)
-                sb.setLength(sb.length() - 1);
-            return sb.toString();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
