f930f9ea6103f6994f22dca7b8cc32521f9ff3a1
Michele Sciabarra <michele+youtrack@sciabarra.com>
2015-06-03 18:11:14 +0100

    removed 762

--- build762.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd core
-call sbt -Dver=7.5.0 "publish-local" 
-cd ..
-cd api
-call sbt -Dver=7.5.0 "publish-local"
-cd ..
--- build762.sh
+++
@@ -1,6 +0,0 @@
-cd core
-sbt -Dver=7.5.0 "publish-local"
-cd ..
-cd api
-sbt -Dver=7.5.0 "publish-local" 
-cd ..
